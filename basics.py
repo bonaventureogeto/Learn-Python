46700060f3ebf9bddf74888d6daf2227f2922b89
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-01-12 08:37:12 +0300

    added changes

--- basics.py
+++ basics.py
@@ -5,3 +5,27 @@
 # mostly used comment
 
 # comments are great for documenting your code
+
+# hello world
+
+print("Hello, World!")
+
+# arithmetic
+print(29+6)
+print(5*3)
+
+print(10 % 8) #modulus
+
+# exponentiation
+print(5**2)
+
+# assignment operators
+
+# variable
+
+numbers = 100
+print(numbers)
+print(numbers + 3)
+print(numbers * 3)
+print(numbers % 3)
+
