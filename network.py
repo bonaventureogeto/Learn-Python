83e48b49b89f1f0a9e8f79895f78e68a56bba26b
Bonaventure <bonaogeto1@gmail.com>
2023-04-03 17:58:35 +0300

    requests

--- network.py
+++ network.py
@@ -1,6 +1,28 @@
 import requests
+import urllib.request
 
 response = requests.get("https://github.com/")
 
 print(response.status_code)
-print(response.content)
+# print(response.content)
+
+data = {'name': 'John', 'age': 30}
+
+response = requests.post('https://httpbin.org/post', data=data)
+
+print(response.status_code)
+# print(response.content)
+
+
+text = "Hello World"
+
+bytes = text.encode('utf-8')
+
+print(bytes)
+
+
+url = "https://github.com"
+response = urllib.request.urlopen(url)
+data = response.read()
+text = data.decode('utf-8')
+print(text)
