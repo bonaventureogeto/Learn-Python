d9c92d0b4997f36bfab960bd64af4ffbe88961a5
Bonaventure <bonaogeto1@gmail.com>
2023-04-04 17:45:25 +0300

    adding to csvfile

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 url = 'https://hojaleaks.com/series/python'
 
@@ -9,5 +10,9 @@ soup = BeautifulSoup(response.content, 'html.parser')
 
 headings = soup.find_all('h1')
 
-for heading in headings:
-    print(heading.text)
+
+with (open('hojaleaks_headings.csv', 'w')) as file:
+    writer = csv.writer(file)
+    writer.writerow(['Headings'])
+    for heading in headings:
+        writer.writerow([heading.text])
