39bfbe9671e624af8af1589cd76bd501600fc701
Bonaventure <bonaogeto1@gmail.com>
2023-04-26 19:39:02 +0300

    file updates

--- files.py
+++ files.py
@@ -1,15 +1,15 @@
-# import os
+import os
 
-# file = open('words.txt', "r")
+file = open('words.txt', "r")
 
-# print(file)
+print(file)
 
-# content = file.read()
+content = file.read()
 
-# print(content)
+print(content)
 
-# with open("words.txt", "r") as file:
-#     print(file.read())
+with open("words.txt", "r") as file:
+    print(file.read())
 
 with open("something.txt", 'r') as thing:
     for i in thing:
@@ -20,3 +20,6 @@ lines = ["Hello", "World", "This", "Is", "A", "List"]
 
 with open("xyz.txt", 'w') as file:
     file.writelines(lines)
+    file.write(
+        "Heyy, I'm an Arsenal Fan. I'm not happy with the way things are going at the club.")
+    file.close()
