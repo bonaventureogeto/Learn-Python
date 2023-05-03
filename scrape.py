437d6ddfdb83e7d5a8c1de9ca868fc7555efb3ab
Bonaventure <bonaogeto1@gmail.com>
2023-05-03 18:21:43 +0300

    scrape updates

--- scrape.py
+++ scrape.py
@@ -8,19 +8,19 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.content, 'html.parser')
 
-images = soup.find_all('img', class_='post-cover')
+headings = soup.find_all('h1', class_='blog-post-card-title')
 
 # for image in images:
 #     print(image['src'])
 
-with (open('hojaleaks_images.csv', 'w')) as file:
-    writer = csv.writer(file)
-    writer.writerow(['images'])
-    for image in images:
-        writer.writerow(image['src'])
-
-# with (open('hojaleaks_headings.csv', 'w')) as file:
+# with (open('hojaleaks_images.csv', 'w')) as file:
 #     writer = csv.writer(file)
-#     writer.writerow(['Headings'])
-#     for heading in headings:
-#         writer.writerow([heading.text])
+#     writer.writerow(['images'])
+#     for image in images:
+#         writer.writerow(image['src'])
+
+with (open('hojaleaks_headings.csv', 'w')) as file:
+    writer = csv.writer(file)
+    writer.writerow(['Headings'])
+    for heading in headings:
+        writer.writerow([heading.text])
