141fb57e13c646bd956e5e61ad3d6f10696ee692
Bonaventure <bonaogeto1@gmail.com>
2023-05-04 17:42:07 +0300

    updates

--- api.py
+++ api.py
@@ -1,9 +1,20 @@
 import requests
 
-api_key = "YOUR-API-KEY"
-url = f"http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid={api_key}"
+api_key = "cbdca0e689dcf8e94ed5633cc78e03d4"
+
+location = input("Enter location: ")
+
+url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}"
+
+# url2 = "https://jsonplaceholder.typicode.com/photos"
 
 response = requests.get(url)
+
+try:
+    response.raise_for_status()
+except requests.exceptions.HTTPError as error:
+    print(f"An error occurred: {error}")
+
 data = response.json()
 
 print(data)
