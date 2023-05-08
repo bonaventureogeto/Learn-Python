bcf1f84a5d8de2199b88fefb10a3e582f28e87c3
Bonaventure <bonaogeto1@gmail.com>
2023-05-08 18:13:16 +0300

    updates

--- oop.py
+++ oop.py
@@ -22,33 +22,64 @@
 # myCar.drive()
 
 
-class Person:
-    def __init__(self, name, age, height):
-        self.name = name
-        self.age = age
-        self.height = height
+# class Person:
+#     def __init__(self, name, age, height):
+#         self.name = name
+#         self.age = age
+#         self.height = height
 
-    def who(self):
+#     def who(self):
+#         print(
+#             f"My name is {self.name} and I'm {self.age} years old and {self.height}cm tall.")
+
+#     def sleep(self):
+#         print("John is sleeping soundly...")
+
+
+# myPerson = Person("John", 25, 180)
+
+# myPerson.sleep()
+
+
+# class MiniPerson(Person):
+#     def __init__(self, name, age, height):
+#         super().__init__(name, age, height)
+
+#     def sleep(self):
+#         print(f"{self.name} is sleeping soundly...")
+
+
+# myMiniPerson = MiniPerson("Grace", 55, 120)
+
+# myMiniPerson.sleep()
+
+
+# bank
+
+class BankAccount:
+    def __init__(self, name, balance):
+        self.name = name  # this(self)
+        self.balance = balance
+
+    def check_balance(self):
+        print(f"Hey {self.name}, Your current balance is ${self.balance}.")
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Hey {self.name}, Your current balance is ${self.balance}.")
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        print(f"Hey {self.name}, Your current balance is ${self.balance}.")
+
+    def appreciation(self):
         print(
-            f"My name is {self.name} and I'm {self.age} years old and {self.height}cm tall.")
-
-    def sleep(self):
-        print("John is sleeping soundly...")
+            f"Thank you for banking with The Bank of Scotland! You are a valued customer!")
 
 
-myPerson = Person("John", 25, 180)
+myBank = BankAccount("Ogah", 89500)
 
-myPerson.sleep()
-
-
-class MiniPerson(Person):
-    def __init__(self, name, age, height):
-        super().__init__(name, age, height)
-
-    def sleep(self):
-        print(f"{self.name} is sleeping soundly...")
-
-
-myMiniPerson = MiniPerson("Grace", 55, 120)
-
-myMiniPerson.sleep()
+myBank.check_balance()
+myBank.deposit(5000)
+myBank.withdraw(10000)
+myBank.appreciation()
