bd7e0aa19972b8ae87bc2fa926e23110914f8fa2
Bonaventure <bonaogeto1@gmail.com>
2023-05-09 18:05:49 +0300

    updates

--- regex.py
+++ regex.py
@@ -1,7 +1,16 @@
 import re
 
-my_txt = 'freeCodeCamp'
-my_regex_1 = '^freecodecamp$'
+# my_txt = 'freeCodeCamp'
+# my_regex_1 = '^freecodecamp$'
 
-res = re.match(my_regex_1, my_txt)  # using the match() method
-print(res)  # returns None
+# res = re.match(my_regex_1, my_txt)  # using the match() method
+# print(res)  # returns None
+
+pattern = re.compile('^Hello')
+
+results = pattern.sub('Kenya', 'Hello World, Hello Python, Hello Regex')
+
+print(results)
+
+# for result in results:
+#     print(result.start(), result.end(), result.group())
