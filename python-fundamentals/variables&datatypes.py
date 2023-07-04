cd876c0a36fadbb899275ff7e4da074debc143f1
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-07-04 17:36:49 +0300

    variables and datatypes in python

---
+++ python-fundamentals/variables&datatypes.py
@@ -0,0 +1,8 @@
+name = "Jane Doe"  # String
+age = 45  # Integer
+height = 1.65  # Float
+
+print(name)
+print(age)
+print("Hello, my name is " + name + " and I am " +
+      str(age) + " years old.")  # Concatenation
