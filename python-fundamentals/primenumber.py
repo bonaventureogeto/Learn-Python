c8b387506c4c3ccab23266feabf23fac5ef98d0a
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-07-05 17:12:41 +0300

    prime numbers between 1 and 101

---
+++ python-fundamentals/primenumber.py
@@ -0,0 +1,10 @@
+# challenge: prime numbers
+# write a program that prints all prime numbers between 1 and 100
+
+for i in range(1, 101):
+    if i > 1:
+        for j in range(2, i):
+            if (i % j) == 0:
+                break
+        else:
+            print(i)
