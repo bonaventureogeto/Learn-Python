11f6f2a5cce6ab627ccaef52d791f290761de6d1
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-07-05 18:14:56 +0300

    updates

--- python-fundamentals/functions.py
+++ python-fundamentals/functions.py
@@ -25,3 +25,16 @@ numbers(10000)
 
 # challenge 2: prime numbers
 # write a function that prints all prime numbers between 1 and n
+
+
+def prime(n):
+    for i in range(1, n + 1):
+        if i > 1:
+            for j in range(2, i):
+                if (i % j) == 0:
+                    break
+                else:
+                    print(i)
+
+
+prime(50)
--- python-fundamentals/primenumber.py
+++ python-fundamentals/primenumber.py
@@ -7,5 +7,5 @@ for i in range(1, 101):
         for j in range(2, i):
             if (i % j) == 0:
                 break
-        else:
-            print(i)
+            else:
+                print(i)
