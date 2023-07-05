ebadb3c098326b247e6d099f6a7dca677abad83f
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-07-05 18:07:42 +0300

    functions in pyhton

---
+++ python-fundamentals/functions.py
@@ -0,0 +1,27 @@
+def square(x):
+    return x * x
+
+
+square(20)
+
+
+def greeting():
+    print("Hello, World!")
+
+
+greeting()
+
+
+def numbers(n):
+    for i in range(1, n + 1):
+        if i % 2 == 0:
+            print(i)
+
+
+numbers(10000)
+
+# challenge 1: grading program
+# write a function that takes in the score of a student and returns/prints the grade of the student
+
+# challenge 2: prime numbers
+# write a function that prints all prime numbers between 1 and n
