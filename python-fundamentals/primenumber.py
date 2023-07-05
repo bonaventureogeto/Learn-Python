1cbe34aeec5973765f2399de0500f1164fcc478a
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-07-05 17:21:30 +0300

    comment

--- python-fundamentals/primenumber.py
+++ python-fundamentals/primenumber.py
@@ -1,6 +1,7 @@
 # challenge: prime numbers
 # write a program that prints all prime numbers between 1 and 100
 
+# prime numbers are numbers that are only divisible by 1 and itself
 for i in range(1, 101):
     if i > 1:
         for j in range(2, i):
