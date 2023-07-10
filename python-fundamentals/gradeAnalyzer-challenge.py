b475f7158d15f845f0e61037dbea8d20f5326ec7
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-07-10 18:32:35 +0300

    grade analyzer coding challenge starter code

---
+++ python-fundamentals/gradeAnalyzer-challenge.py
@@ -0,0 +1,7 @@
+def grade_analyzer(student_grades, operation):
+    # calculations
+    return
+
+
+grade_analyzer([("Jane", 80.0), ("John", 60.2), ("Elizabeth", 90.9),
+               ("Harry", 70.3), ("Maria", 85.6)], average)
