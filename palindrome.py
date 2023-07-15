1ff4be27537b9f63d4327a1d6fe5c00b1a41596f
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-07-15 12:12:00 +0300

    palindrome checker

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+# check for palindromes in a file
+
+# file = open('words.txt', 'r')
+# for words in file:
+#     words = words.strip()
+#     if(words == words[::-1]):
+#         print(words)
+
+with open('words.txt', 'r') as file:
+    for words in file:
+        words = words.strip()
+        if(words == words[::-1]):
+            print(words)
