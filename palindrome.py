d80db009d84c216962762706c35983d1649b55cf
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-07-22 13:35:02 +0300

    updates

--- palindrome.py
+++ palindrome.py
@@ -9,5 +9,5 @@
 with open('words.txt', 'r') as file:
     for words in file:
         words = words.strip()
-        if(words == words[::-1]):
+        if (words == words[::-1]):
             print(words)
