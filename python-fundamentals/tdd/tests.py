9a14ae6055f2f605949584ed41c74c797e131233
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-07-26 19:51:43 +0300

    writting tests

---
+++ python-fundamentals/tdd/tests.py
@@ -0,0 +1,10 @@
+import unittest
+from factorial import factorial
+
+class TestFactorial(unittest.TestCase):
+    def test_factorial(self):
+        self.assertEqual(factorial(0), 1)
+        self.assertEqual(factorial(1), 1)
+        self.assertEqual(factorial(2), 2)
+        self.assertEqual(factorial(3), 6)
+        self.assertEqual(factorial(4), 24)
