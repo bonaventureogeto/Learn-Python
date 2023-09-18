e71115a99e5204328c105e5d6934805afd474180
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-09-18 19:58:53 +0300

    added intro to python notes

---
+++ Introduction-to-python/variables.py
@@ -0,0 +1,13 @@
+# variables are used to store values in python
+
+# variable declaration & variable initialization
+
+name = "Jude Doe"  # assignment
+
+#  1 + 1 = 2  - value on the left and on the right are equal
+
+greeting = "Hello, guys! Welcome to my YouTube channel."
+
+print(name)
+
+print(greeting)
