d217dd5d3f6cb177ce86f9cbea4a4b0eb1568a4b
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-09-18 20:48:31 +0300

    datatypes and arithmetic operations in Python

---
+++ Introduction-to-python/data-types.py
@@ -0,0 +1,18 @@
+# data types in python
+
+# string
+name = "Chicken Doe"
+
+county = 'Kiambu'
+
+age = "35"
+
+height = "1.89"
+
+# integer
+number = 20
+
+# float
+num = 10.98
+
+print(num)
---
+++ Introduction-to-python/operations.py
@@ -0,0 +1,9 @@
+number1 = 30458
+
+number2 = 3857
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 % number2)  # mudulus - remainder after division
