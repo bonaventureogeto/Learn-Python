913c03f36cf87fdeb2068ee0f1a2a895564da77f
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-10-02 19:17:34 +0300

    data structures in Python

---
+++ Introduction-to-python/data-structures/lists.py
@@ -0,0 +1,15 @@
+# list - store information
+names = ["Jake", "Jack", "Jux"]
+number = [1]
+
+mixed = ["Car", 1, 1.545, True, "Kenya"]
+
+listTuple = [(1, 2), ("name", 12), ("day", 23)] 
+
+mixed[3] = False
+
+print(mixed)
+
+for item in mixed:
+    print(item)
+    
---
+++ Introduction-to-python/data-structures/tuples.py
@@ -0,0 +1,7 @@
+# tuples - store information - ()
+names = ("John", "Jude", "Jack")
+number = (1,)
+
+names[1] = "Jake" #assignment to a tuple
+
+print(names)
