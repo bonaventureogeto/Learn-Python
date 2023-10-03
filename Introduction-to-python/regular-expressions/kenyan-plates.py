f3673bc585d7d66f0e274e9eedb6cd8ca4f3332e
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-10-03 20:20:39 +0300

    regex for kenyan number plates

--- Introduction-to-python/regular-expressions/kenyan-plates.py
+++ Introduction-to-python/regular-expressions/kenyan-plates.py
@@ -3,3 +3,14 @@
 
 # \w - matches any word character (a-z, A-Z, 0-9, _)
 # \s - matches any whitespace character
+
+import re
+
+def matchPlate(text, pattern):
+    result = re.match(pattern, text)
+    print(result)
+
+text = "KCD 376 Q"
+pattern = r"^[A-Z]{3}\s\d{3}\s[A-Z]{1}$"
+
+matchPlate(text, pattern)
