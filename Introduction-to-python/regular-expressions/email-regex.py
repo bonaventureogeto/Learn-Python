afa9a71857370c2d84a9c24e76a32c5d9a8fd962
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-10-03 20:33:43 +0300

    updates

--- Introduction-to-python/regular-expressions/email-regex.py
+++ Introduction-to-python/regular-expressions/email-regex.py
@@ -1,9 +1,9 @@
 import re
 
-email = "0x10x12310101@0x10x1.0x1"
+email = "0x10x1-2067@0x10x1.0x1"
 pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
 
 if re.match(pattern, email):
-    print("Valid email address.")
+    print(email, "is a valid email address.")
 else:
     print("Invalid email address.")
