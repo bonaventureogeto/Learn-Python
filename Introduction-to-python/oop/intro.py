73827753dc137d7ebfb6df12fa907d70665d6efe
Bonaventure Ogeto <bonaogeto1@gmail.com>
2023-10-11 19:39:40 +0300

    inheritance

--- Introduction-to-python/oop/intro.py
+++ Introduction-to-python/oop/intro.py
@@ -1,8 +1,10 @@
 class Person:
-    def __init__(self, name, age, occupation):
+    def __init__(self, name, age, occupation, country, race):
         self.name = name
         self.age = age
         self.occupation = occupation
+        self.country = country
+        self.race = race
         
     def greet(self):
         print("Hello", self.name)
@@ -13,9 +15,24 @@ class Person:
     def vote(self):
         pass
     
-myPerson = Person("Jack", 20, "Lab Technician")
+myPerson = Person("Jack", 20, "Lab Technician", 'Kenya', 'Indian')
 
 print(myPerson.name)
 print(myPerson.age)
 myPerson.greet()
 myPerson.cry()
+
+class Child(Person):
+    def __init__(self, name, age, occupation, country, race, age1, firstname):
+        Person.__init__(self, name, age, occupation, country, race)
+        self.age1 = age1
+        self.firstname = firstname
+        
+    def hello(self):
+        print(f'My name is {self.name}, I am from {self.country}')
+        
+myChild = Child("Jack", 20, "Lab Technician", 'Kenya', 'Indian', 20, 'John')
+
+print(myChild)
+
+print(myChild.hello())
